036f1f45b2ca1209445025e61824f6a31f35c854
jtregoat <johanne.tregoat@imt-atlantique.net>
2020-01-08 13:15:17 +0100

    Update appariement.py

--- appariement.py
+++ appariement.py
@@ -8,3 +8,29 @@ import random
 
 def appariement_population(population):
     random.shuffle(population)
+	
+	
+	
+#On fait un appariement par valeur de Cmax croissant
+def C_pairing(L):
+	L = sorted(L, key = lambda colonnes: colonnes[1])
+
+
+def pairing(L):
+	L = sorted(L, key = lambda colonnes: colonnes[1])
+	n = int(len(L)/2)
+	M=[]
+	for k in range(n):
+		M.append([L[k], L[k+n]])
+	L = M
+	
+	
+#on apparie le Cmax minimal avec le maximal
+def mixed_pairing(L):
+	L = sorted(L, key = lambda colonnes: colonnes[1])
+	m = len(L)
+	n = int(m/2)
+	M=[]
+	for k in range(n):
+		M.append([L[k], L[m-k-1]])
+	L = M
