a7f3546280e844dd7f4a1d9aa0b999d32d80963b
Valentin <valentin.bielecki@gmail.com>
2020-01-16 18:11:55 +0100

    mutation

--- hyperparametres.py
+++ hyperparametres.py
@@ -15,9 +15,11 @@ T = 10
 
 fichier = "tai21.txt"
 
-f = open("test_selection.txt", "w")
-f.write('Test Differentes Selections' +'\n')
+f = open("test_mutation.txt", "w")
+f.write('Test Differentes Valeurs de Mutation' +'\n')
 temps_max = 360 # Temps maximal d'un calcul en secondes
+
+mutations = [1, 5, 10, 15, 20]
 
 population_initiale = generation.generation_aleatoire(fichier, N) # Génération
 population_initiale = sorted(population_initiale, key=lambda ordonnancement: ordonnancement.dur)
@@ -30,76 +32,27 @@ Moy = statistics.mean(solutions_initiales)
 
 f.write('Cmin: '+str(Cmin)+' '+'Cmax: '+str(Cmax)+' '+'Moyenne: '+str(Moy)+'\n')
 
-f.write('P Meilleurs: ')
-meilleures_solutions_p_meilleurs = []
+for r in range(len(mutations)):
+    f.write(str(mutations[r])+'%'+'\n')
+    meilleures_solutions = []
+    for i in range(10): # On fait 10 epoch
+        temps_initial = time.time()
+        C = Cmin
+        population = population_initiale
+        #e = 0
+        while (time.time()-temps_initial < temps_max):
+            appariement.C_pairing(population)
+            population = croisement.croisement_population(population) # Croisement
+            mutation.mutation_population(population, mutations[r]) # Mutation
+            population = selection.selection_population_tournoi(population, T) # Sélection 80% meilleurs
+            #e+=1
+            if C > population[0].dur: # Sauvegarde du meilleur individu
+                C = population[0].dur
+        meilleures_solutions.append(C)
+    f.write(str(statistics.mean(meilleures_solutions))+'\n')
 
-for i in range(10): # On fait 10 epoch
-    temps_initial = time.time()
-    C = Cmin
-    population = population_initiale
-    #e = 0
-    while (time.time()-temps_initial < temps_max):
-        appariement.C_pairing(population)
-        population = croisement.croisement_population(population) # Croisement
-        mutation.mutation_population(population, 10) # Mutation
-        population = selection.selection_population_p_meilleurs(population, p) # Sélection 80% meilleurs
-        #e+=1
-        if C > population[0].dur: # Sauvegarde du meilleur individu
-            C = population[0].dur
-    meilleures_solutions_p_meilleurs.append(C)
-f.write(str(statistics.mean(meilleures_solutions_p_meilleurs))+'\n')
-
-for j in range(len(meilleures_solutions_p_meilleurs)):
-    f.write(str(meilleures_solutions_p_meilleurs[j])+' ')
-f.write('\n')
-#f.write('Nombre de generation: '+str(e)+'\n')
-
-f.write('Roulette: ')
-meilleures_solutions_roulette = []
-
-for i in range(10): # On fait 10 epoch
-    temps_initial = time.time()
-    C = Cmin
-    population = population_initiale
-    #e = 0
-    while (time.time()-temps_initial < temps_max): 
-        appariement.C_pairing(population)
-        population = croisement.croisement_population(population) # Croisement
-        mutation.mutation_population(population, 10) # Mutation
-        population = selection.selection_population_roulette(population)
-        #e+=1
-        if C > population[0].dur: # Sauvegarde du meilleur individu
-            C = population[0].dur
-    meilleures_solutions_roulette.append(C)
-f.write(str(statistics.mean(meilleures_solutions_roulette))+'\n')
-
-for j in range(len(meilleures_solutions_roulette)):
-    f.write(str(meilleures_solutions_roulette[j])+' ')
-f.write('\n')
-#f.write('Nombre de generation: '+str(e)+'\n')
-
-f.write('Tournois: ')
-meilleures_solutions_tournois = []
-
-for i in range(10): # On fait 10 epoch
-    temps_initial = time.time()
-    C = Cmin
-    population = population_initiale
-    #e = 0
-    while (time.time()-temps_initial < temps_max): 
-        population = croisement.croisement_population(population) # Croisement
-        mutation.mutation_population(population, 10) # Mutation
-        population = selection.selection_population_tournoi(population,T) # Sélection par tounois de T participants
-        appariement.C_pairing(population)
-        #e+=1
-        if C > population[0].dur: # Sauvegarde du meilleur individu
-            C = population[0].dur
-    meilleures_solutions_tournois.append(C)
-f.write(str(statistics.mean(meilleures_solutions_tournois))+'\n')
-
-for j in range(len(meilleures_solutions_tournois)):
-    f.write(str(meilleures_solutions_tournois[j])+' ')
-f.write('\n')
-#f.write('Nombre de generation: '+str(e)+'\n')
-
+    for j in range(len(meilleures_solutions)):
+        f.write(str(meilleures_solutions[j])+' ')
+    f.write('\n')
+    #f.write('Nombre de generation: '+str(e)+'\n')
 f.close()
