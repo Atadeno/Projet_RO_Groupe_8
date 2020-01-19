a3484ed4261ff06afe57a2835b6fab424ac0ceaa
Valentin <valentin.bielecki@gmail.com>
2020-01-19 22:58:08 +0100

    Update generation.py

--- generation.py
+++ generation.py
@@ -7,7 +7,7 @@ import numpy as np
 
 ### Génération ###
 
-def generation_Heuristique (mon_fichier_txt, N):
+def generation_Heuristique(mon_fichier_txt, N):
     flow_shop = flowshop.Flowshop()
     ordo = flow_shop.definir_par(mon_fichier_txt) # Construction d'un problème avec un fichier .txt
     nb_machines = ordo.nb_machines
@@ -15,25 +15,25 @@ def generation_Heuristique (mon_fichier_txt, N):
     nbr_Jobs=len(liste_job)
     
     t=[] #processing time matrix
-    for job in liste_job
+    for job in liste_job:
         t.append(job.duree_op)
         
-    palmIndex=[0 for i in range(n)] #index de pente de Palmer de chaque tache
+    palmIndex=[0 for i in range(nbr_Jobs)] #index de pente de Palmer de chaque tache
 
     for i in range(1,nbr_Jobs):
         sum=0
         for j in range (1,nb_machines):
-            sum=sum+(nb_machines-2*j+1)*t[i,j]
+            sum=sum+(nb_machines-2*j+1)*t[i][j]
         palmIndex[i]=sum
 
     johnIndex=[0 for i in range(nbr_Jobs)] #index de pente de Johnson de chaque tache
     for i in range (1,nbr_Jobs):
         mi=10000
         for j in range (1,nb_machines-1):
-            if t[i,j]+t[i,j+1]<mi:
-                mi=t[i,j]+t[i,j+1]
+            if t[i][j]+t[i][j+1]<mi:
+                mi=t[i][j]+t[i][j+1]
         signe=1
-        if (t[i,1]-t[i,nb_machines])<0:
+        if (t[i][1]-t[i][nb_machines])<0:
             signe=-1
         johnIndex[i]=palmIndex[i]-signe/mi
 
@@ -68,3 +68,4 @@ def generation_aleatoire(mon_fichier_txt, N):
         i-=-1
     return population_initiale
 
+pop = generation_Heuristique("jeu1.txt",2)
